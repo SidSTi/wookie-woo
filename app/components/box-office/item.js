517cda4a471b622f6ea79b863e8ff222858a5300
Sethi, Sid <Sid_Sethi@gallup.com>
2019-05-16 12:41:05 -0500

    [ww-0004]: implement films route and components

---
+++ app/components/box-office/item.js
@@ -0,0 +1,33 @@
+import Component from '@ember/component';
+
+export default Component.extend({
+
+  /**
+   * Set the default class names for this component
+   *
+   * @override
+   * @public
+   * @property classNames
+   * @type {string[]}
+   */
+  classNames: ['box-office__item'],
+
+  /**
+   * Component cache.
+   *
+   * @public
+   * @property model
+   * @type {Object[]}
+   */
+  model: null,
+
+  /**
+   * Default tag name for this component.
+   *
+   * @override
+   * @public
+   * @property tagName
+   * @type {string}
+   */
+  tagName: 'section'
+});
